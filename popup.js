aaf4f49384b242d31fece3018d00b90fbab59b2a
Akhil Thampy <akhilthampy@yahoo.com>
2015-10-26 12:18:13 -0500

    Trying to get insertCSS to work

--- popup.js
+++ popup.js
@@ -1,5 +1,17 @@
-
-chrome.tabs.query({active:false}, function(tabs){
+/*
+ * tabs is an array of all fb tabs.
+ * */
+function killfb(tabs){
+	for(i in tabs){
+		var obj ={
+			code: "*{display:none;}",
+			allFrames: true
+		}; 
+		chrome.tabs.insertCSS(tabs[i].id, obj);
+	}
+}
+chrome.tabs.query({active:false, url:'*://www.facebook.com/'},killfb);
+/*chrome.tabs.query({active:false, url:'*://www.facebook.com/'}, function(tabs){
 	var div = document.getElementById('status');
 	var content = document.createElement("ul");
 	console.log(tabs);
@@ -9,6 +21,4 @@ chrome.tabs.query({active:false}, function(tabs){
 		content.appendChild(li);
 	}
 	div.appendChild(content);
-});
-
-
+});*/
