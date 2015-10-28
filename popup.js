41fb62d55474a7cd50dc840579e70417df186ac3
Akhil Thampy <akhilthampy@yahoo.com>
2015-10-28 12:36:04 -0500

    Got insertCSS working.

--- popup.js
+++ popup.js
@@ -4,21 +4,10 @@
 function killfb(tabs){
 	for(i in tabs){
 		var obj ={
-			code: "*{display:none;}",
-			allFrames: true
+			code: "*{display:none;}"
 		}; 
+		console.log(tabs[i].id +"\t"+ tabs[i].url);
 		chrome.tabs.insertCSS(tabs[i].id, obj);
 	}
 }
-chrome.tabs.query({active:false, url:'*://www.facebook.com/'},killfb);
-/*chrome.tabs.query({active:false, url:'*://www.facebook.com/'}, function(tabs){
-	var div = document.getElementById('status');
-	var content = document.createElement("ul");
-	console.log(tabs);
-	for(i in tabs){
-		var li = document.createElement("li");
-		li.appendChild(document.createTextNode(tabs[i].id +"\t"+ tabs[i].url));
-		content.appendChild(li);
-	}
-	div.appendChild(content);
-});*/
+chrome.tabs.query({active:false, url:'https://www.facebook.com/'},killfb);
